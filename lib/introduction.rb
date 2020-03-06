259a1e55ff35a31750fbed0a3759e521660d481a
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-03-06 15:49:23 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,17 +1,7 @@
-def introduction
-end
-
-def introduction(name)
-end
-
-def introduction(name)
-  puts "Hello! my name is #{name}."
-end
-
 def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
 def introduction_with_language(name, language)
-  puts"Hello #{name}!, what is your fav #{language}?"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
